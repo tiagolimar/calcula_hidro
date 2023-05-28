4dad54b4494e2ec45bc0d448fe6419c827ef2d88
tiagolimar <tiagolima.r2@gmail.com>
2023-05-28 16:03:34 -0300

    criacao da pag de dimensionamento nbr 10844

---
+++ js/nbr10844.js
@@ -0,0 +1,7 @@
+function ocultar_tabelas(){
+    if (referencia.style.display == 'flex'){
+        referencia.style.display = 'none';
+    }else{
+        referencia.style.display = 'flex';
+    }
+}
