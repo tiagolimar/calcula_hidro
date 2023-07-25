9145f31cfb80bfa79ad4b09e863835d20011ecf1
tiagolimar <tiagolima.r2@gmail.com>
2023-07-25 20:46:57 -0300

    alteracao em InputNumber

--- calcula_hidro/src/components/InputNumber.jsx
+++ calcula_hidro/src/components/InputNumber.jsx
@@ -2,7 +2,6 @@ import { Col } from "./Col";
 
 export const InputNumber = (props) => {
     const type = props.type ? props.type : "text";
-    const value = props.value ? props.value : "0.000";
     const step = props.step ? props.step : 0.1;
     const min = props.min ? props.min : 0.0;
     const placeholder = props.placeholder ? props.placeholder : 'Digite...';
@@ -12,7 +11,7 @@ export const InputNumber = (props) => {
     return (
         <Col width={width}>
             <input type={type}
-                value={value}
+                value={props.value}
                 step={step}
                 min={min}
                 id={props.id}
