6a778cce06bc3ae99bed95575fa2fd511645c480
tiagolimar <tiagolima.r2@gmail.com>
2023-08-04 21:13:20 -0300

    conclusão da sessão tubo

--- calcula_hidro/src/components/InputNumber.jsx
+++ calcula_hidro/src/components/InputNumber.jsx
@@ -2,8 +2,6 @@ import { Col } from "./Col";
 
 export const InputNumber = (props) => {
     const type = props.type ? props.type : "text";
-    const step = props.step ? props.step : 0.1;
-    const min = props.min ? props.min : 0.0;
     const placeholder = props.placeholder ? props.placeholder : 'Digite...';
     const width = props.width ? props.width : '';
     const disabled = props.disabled ? props.disabled : false;
@@ -12,8 +10,7 @@ export const InputNumber = (props) => {
         <Col width={width}>
             <input type={type}
                 value={props.value}
-                step={step}
-                min={min}
+                min={0}
                 id={props.id}
                 className={`form-control ${width}`}
                 placeholder={placeholder}
--- calcula_hidro/src/components/Tubos.jsx
+++ calcula_hidro/src/components/Tubos.jsx
@@ -15,9 +15,15 @@ export const Tubos = () => {
     const [DI, setDI] = useState(0);
     const [indexDN,setIndexDN] = useState(0);
 
-    const [clout,setClout] = useState(0)
+    const [clout,setClout] = useState(0);
+    const [flowMeter,setFlowMeter] = useState(0);
+    const [flowLiter,setFlowLiter] = useState(0);
+    const [lengthTotal,setLengthTotal] = useState(0);
+    const [lostTotal,setLostTotal] = useState(0);
+    const [velocity,setVelocity] = useState(0);
+    const [lostUnit,setLostUnit] = useState(0);
 
-    useEffect(function main() {
+    useEffect(()=>{
         (async () => {
             const data = await getPipes("./src/data/nbr_5626_tubos.json");
             setListPipes(data);
@@ -26,7 +32,7 @@ export const Tubos = () => {
         })();
     }, []);
 
-    useEffect(function updateListDN() {
+    useEffect(()=>{
             if (listPipes.length > 0) {
                 for (const pipe of listPipes) {
                     if (pipe.material == material) {
@@ -34,13 +40,13 @@ export const Tubos = () => {
                         setListDI(pipe.dn_interno_mm);
                         setDN(pipe.dn_comercial_mm[0]);
                         setDI(pipe.dn_interno_mm[0]);
-                        setIndexDN(1)
+                        setIndexDN(0);
                     }
                 }
             }
         },[material, listDI, listDN, listPipes]);
 
-    useEffect(function updateDI() {
+    useEffect(()=>{
             for (const i in listDN) {
                 if (listDN[i] == DN) {
                   setDI(listDI[i]);
@@ -49,6 +55,37 @@ export const Tubos = () => {
             }
         },[DN, listDI, listDN]);
 
+    useEffect(()=>{
+        const flow = eval(0.3*clout**0.5).toFixed(3);
+        if (flow != flowLiter){
+            setFlowLiter(flow);
+            setFlowMeter(eval(0.3*clout**0.5/1000*3600).toFixed(3));
+        }
+    },[clout,flowLiter])
+    
+    useEffect(()=>{
+        const flow = eval(flowMeter*1000/3600).toFixed(3);
+        if(flow != flowLiter){
+            setFlowLiter(flow);
+            setClout(eval((flowLiter/0.3)**2).toFixed(3));
+        }
+    },[flowMeter,flowLiter])
+    
+    useEffect(()=>{
+        const flow_MpS = flowMeter/3600;
+        const diameter = DI/1000;
+
+        if (flow_MpS){
+            setVelocity((4*flow_MpS)/(Math.PI*diameter**2).toFixed(3));
+            setLostUnit(0.000859*(flow_MpS**(1.75))/(diameter**4.75));
+        }
+
+        if(lostUnit*lengthTotal){
+            setLostTotal(lostUnit*lengthTotal);
+        }
+        
+    },[flowMeter,DI,lengthTotal])
+    
     return listPipes.length > 0 ? (
         <Card title="Tubos">
             <Col>
@@ -58,16 +95,27 @@ export const Tubos = () => {
                     value={material}
                     onChange={(e) => setMaterial(e.target.value)}
                 />
-
                 <InputFormNumber 
                 title="Peso R."
                 value={clout}
                 onChange={e => setClout(e.target.value)}
                 />
-
-                <InputFormNumber disabled title="DI" unit=" (mm)" value={DI} />
-                <InputFormNumber disabled title="V" unit=" (m/s)" />
-                <InputFormNumber disabled title="Ju" unit=" (m/m)" />
+                <InputFormNumber
+                    title="Lt"
+                    unit=" (m)"
+                    value={lengthTotal}
+                    onChange={e=>setLengthTotal(e.target.value)}
+                />
+                <InputFormNumber disabled
+                    title="DI"
+                    unit=" (mm)"
+                    value={DI}
+                />
+                <InputFormNumber disabled
+                    title="Ju"
+                    unit=" (m/m)"
+                    value={lostUnit}
+                />
             </Col>
             <Col>
                 <SelectForm
@@ -78,19 +126,31 @@ export const Tubos = () => {
                     onChange={e => setDN(e.target.value)}
                     selectedIndex = {indexDN}
                 />
-
-                <InputFormNumber title="Qm" unit=" (m³/h)" />
-                <InputFormNumber disabled title="Ql" unit=" (l/s)" />
-                <InputFormNumber disabled title="Lt" unit=" (m)" />
-                <InputFormNumber disabled title="Jt" unit=" (mca)" />
+                <InputFormNumber
+                    title="Qm"
+                    unit=" (m³/h)"
+                    value={flowMeter}
+                    onChange={e=>{setFlowMeter(e.target.value)}}
+                />
+                <InputFormNumber
+                    disabled
+                    title="Ql"
+                    unit=" (l/s)"
+                    value={flowLiter}
+                />
+                <InputFormNumber
+                    disabled
+                    title="V"
+                    unit=" (m/s)"
+                    value={velocity}
+                />
+                <InputFormNumber
+                    disabled
+                    title="Jt"
+                    unit=" (mca)"
+                    value={lostTotal}
+                />
             </Col>
         </Card>
     ) : null;
 };
-
-Qm	vazaoMetros
-Ql	vazaoLitros
-Lt	lengthTotal
-Jt	lostTotal
-V	velocitypesoin
-Ju	lostUnit
